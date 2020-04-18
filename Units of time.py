ca8e8302bda39780388ce4f88d4eed1c06f121a3
ML3636 <markus.lam22@ycdsbk12.ca>
2020-04-17 22:17:49 -0400

    first commit

---
+++ Units of time.py
@@ -0,0 +1,13 @@
+Days = int(input("please print the amount of Days"))
+Hours = int(input("please print the amount of Hours"))
+Minutes = int(input("please print the amount Minutes"))
+Seconds = int(input("please print the amount of Seconds"))
+
+SID = Days*86400
+SIH = Hours*3600
+SIM = Minutes*60
+
+print (SID + SIH + SIM + Seconds "seconds")
+
+
+
